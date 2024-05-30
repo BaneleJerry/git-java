204cc4c2a307271d5c1c1e3435a60983bba1b84d
Banele Thabede <banelethabede@outlook.com>
2024-05-30 12:22:38 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,22 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+  public static void main(String[] args) {
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
 
     // Uncomment this block to pass the first stage
     final String command = args[0];
-    
+
     switch (command) {
       case "init" -> {
         final File root = new File(".git");
         new File(root, "objects").mkdirs();
         new File(root, "refs").mkdirs();
         final File head = new File(root, "HEAD");
-    
+
         try {
           head.createNewFile();
           Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
@@ -25,7 +25,28 @@ public class Main {
           throw new RuntimeException(e);
         }
       }
+
+      case "cat-file" -> {
+        String option = args[1];
+        if (option.equalsIgnoreCase("-p")) {
+          final String blobSha = args[2];
+          catFile(blobSha);
+        }
+      }
       default -> System.out.println("Unknown command: " + command);
+    }
+  }
+
+  private static void catFile(String blobSha) {
+    String fileName = String.format(".git/objects/%s/%s", blobSha.substring(0, 2), blobSha.substring(2));
+    try(BufferedReader reader = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(fileName))))){
+      String line = reader.readLine();
+      System.out.print(line.substring(line.indexOf('\0') + 1));
+      while ((line = reader.readLine()) != null){
+        System.out.println(line);
+      }
+    }catch (IOException e){
+      e.printStackTrace();
     }
   }
 }
